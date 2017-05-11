e0848fd2618d291ee923fc63c195856ae620d4e5
yossi2010 <yossicohen2000@gmail.com>
2017-05-11 17:42:51 +0300

    k
    Signed-off-by: yossi2010 <yossicohen2000@gmail.com>

--- src/colorado/Scripts/CMD_VEL_Direct_Control.py
+++ src/colorado/Scripts/CMD_VEL_Direct_Control.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 import roslib
 import rospy
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = ""
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, # Internet
+             socket.SOCK_DGRAM) # UDP
+
+
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
 pubt = rospy.Publisher('/colorado/Driving/Throttle', Float64, queue_size=10)
@@ -16,6 +30,8 @@ def callback(msg):
     rospy.loginfo(Throttle)
     pubt.publish(Float64(Throttle))
     pubs.publish(Float64(-Steer))
+    MESSAGE ="%f %f" % (Throttle, -Steer)
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 def listener():
     
